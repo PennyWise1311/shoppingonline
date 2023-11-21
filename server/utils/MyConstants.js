dc41a700a535fae55e2c316d5ea35143505abcd4
yeuchithuyvailon <pennywise131103@gmail.com>
2023-11-21 19:01:23 +0700

    first Project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.rgt2st5.mongodb.net',
+  DB_USER: 'hihihi',
+  DB_PASS: '123456Son.',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'cuuu',
+  JWT_EXPIRES: '3600000000', // in milliseconds
+  EMAIL_USER: 'pennywise131103@gmail.com', // gmail service
+  EMAIL_PASS: 'pipr awct zlje myqv'
+};
+module.exports = MyConstants;
